e796551a3ba6fdb089d1a19e955113dcfb3dae27
Emma <MarmadileManteater@proton.me>
2022-12-13 09:28:12 -0500

    Change header to be styled text

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,16 @@
-import { component$ } from '@builder.io/qwik'
-import headerImage from '../../images/marmadilemanteater.png'
+import { component$, useStylesScoped$ } from '@builder.io/qwik'
+import styles from './header.css'
+/* import headerImage from '../../images/marmadilemanteater.png' */
 
 export default component$(() => {
-
+  useStylesScoped$(styles)
   return (
     <header>
-      <img src={headerImage} style='margin-left: auto; margin-right: auto;' />
+      <h2 class='text-4xl sm:text-6xl md:text-7xl lg:text-8xl p-8 text-center pb-10'>
+        <span class='red text-outline text-red-500 drop-shadow-lg'>Marmadile</span> 
+        <span class='green text-outline text-green-500'>Manteater</span>
+      </h2>
+      {/* <img src={headerImage} style='margin-left: auto; margin-right: auto;' /> */}
     </header>
   )
 })
