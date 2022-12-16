cd4ddeb2219dc2cd51cbc3b30cef0a15942424f7
Emma <MarmadileManteater@proton.me>
2022-12-15 19:01:04 -0500

    Change text color of light mode buttons to be more accessible

--- src/components/project-button/project-button.tsx
+++ src/components/project-button/project-button.tsx
@@ -11,7 +11,7 @@ export default component$(({link, target, index = 0} : IProps) => {
     <a
       href={link}
       target={target}
-      class={`p-3 inline-block hover:underline ${index % 2 === 0?'dark:bg-zinc-900':'dark:bg-zinc-700'} ${index % 2 === 0?'bg-blue-400':'bg-red-400'}  text-white dark:bg-zinc-600 rounded-lg mr-5 mb-3`}
+      class={`p-3 inline-block hover:underline ${index % 2 === 0?'dark:bg-zinc-900':'dark:bg-zinc-700'} ${index % 2 === 0?'bg-blue-400':'bg-red-400'}  text-black dark:text-white dark:bg-zinc-600 rounded-lg mr-5 mb-3`}
     ><Slot /> &raquo;</a>
   )
 })
