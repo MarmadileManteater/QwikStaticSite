79ee28af65aceea58101a3b01c1821141ddd6d03
Emma <MarmadileManteater@proton.me>
2022-12-24 05:45:09 -0500

    Adjust padding on content-cards when thumbnail not present

--- src/components/content-card/content-card.tsx
+++ src/components/content-card/content-card.tsx
@@ -18,7 +18,7 @@ interface IProps {
 export default component$(({ title, titleLink, summary, thumbnail, tags, tagData, index = 0 } : IProps) => {
   useStylesScoped$(styles)
   return (
-    <div class={`content-card md:pt-0 pt-4 outer-grid ${index % 2 === 0?'bg-zinc-100':'bg-white'} ${index % 2 === 0?'dark:bg-zinc-800':'dark:bg-zinc-900'} dark:text-white`}>
+    <div class={`content-card md:pt-0 ${thumbnail?'pt-4':'pt-0'} outer-grid ${index % 2 === 0?'bg-zinc-100':'bg-white'} ${index % 2 === 0?'dark:bg-zinc-800':'dark:bg-zinc-900'} dark:text-white`}>
       {thumbnail?<>
         <div class='p-4 pr-0 image-grid'>
           <a href={titleLink}><Image src={thumbnail} alt={title} style='max-height: 153px; display: inline-block;' /></a>
