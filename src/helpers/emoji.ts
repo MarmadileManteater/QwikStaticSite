0d8c858702c065fef67db187445dcf3d05cbbe4e
Emma <MarmadileManteater@proton.me>
2023-01-04 11:13:41 -0500

    Add edge case logic for emoji

--- src/helpers/emoji.ts
+++ src/helpers/emoji.ts
@@ -3,25 +3,36 @@
 import emojiUnicode from 'emoji-unicode'
 
 export function emojiToOtherMoji (givenEmoji : string) : string {
-  const unicode = emojiUnicode(givenEmoji).toLowerCase().replaceAll(' ', '-')
+  let unicode = emojiUnicode(givenEmoji).toLowerCase().replaceAll(' ', '-')
+  switch (unicode) {
+  // special cases:
+  case '1f937-200d-2640':// what this actually means is:
+    unicode = '1f937-200d-2640-fe0f'
+    break
+  default:
+    break
+  }
   return `https://twemoji.maxcdn.com/v/14.0.2/72x72/${unicode}.png`
 }
 
 export function convertEmojiToImages(html : string) : string {
   // Convert to a set and back
-  const emojiMatches = Array.from(html.matchAll(/[\p{Emoji}\u200d]+/gu))
+  // 🤔 I can't figure out how to generalize matching the trans flag
+  const emojiMatches = Array.from(html.matchAll(/🏳️‍⚧️|(\p{Emoji}(\u200d\p{Emoji})*)/gu))
   const listOfEmojiFound = emojiMatches.map((match) => {
+    const unicode = html.codePointAt(match.index as number)?.toString(16)
     return {
-      index: match.index, 
-      unicode: html.codePointAt(match.index as number)?.toString(16),
-      emoji: match[0]
+      index: match.index,
+      unicode,
+      emoji: match.at(0)
     }
   }).filter(({unicode}) => (unicode as string).length > 2).map(({emoji}) => emoji)
   // Convert the list to a set because sets can't have duplicate entries
   const emojiFound = Array.from(new Set(listOfEmojiFound))
   for (let i = 0; i < emojiFound.length; i++) {
     const emoji = emojiFound[i]
-    html = html.replace(new RegExp(`([^"]*)${emoji}`, 'g'), `$1<span class='inline-block emoji'><img src="${emojiToOtherMoji(emoji)}" alt="${emoji}" /></span>`)
+    //console.log(`<span class='inline-block emoji'><img src="${emojiToOtherMoji(emoji)}" alt="${emoji}" /></span>`)
+    html = html.replace(new RegExp(`([^"]*)${emoji}`, 'g'), `$1<span class='inline-block emoji'><img src="${emojiToOtherMoji(emoji as string)}" alt="${emoji}" /></span>`)
   }
   return html
 }
