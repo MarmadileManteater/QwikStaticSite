cf85e99f4691a7e702b9c722add91fb980b46197
Emma <MarmadileManteater@proton.me>
2023-01-05 11:01:28 -0500

    Modify colours

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -9,8 +9,8 @@ export default component$(() => {
     <header>
       <Link href='/'>
         <h2 class='text-4xl sm:text-6xl md:text-7xl lg:text-8xl p-8 text-center pb-10'>
-          <span class='red text-outline text-red-500 drop-shadow-lg'>Marmadile</span> 
-          <span class='green text-outline text-green-500'>Manteater</span>
+          <span class='red text-outline text-blue-500 drop-shadow-lg'>Marmadile</span> 
+          <span class='green text-outline text-blue-400'>Manteater</span>
         </h2>
       </Link>
       {/* <img src={headerImage} style='margin-left: auto; margin-right: auto;' /> */}
