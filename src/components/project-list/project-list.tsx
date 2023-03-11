b0bce2e12f31d790a21cd95a10f0ebdce59ca49e
Emma <MarmadileManteater@proton.me>
2023-03-11 18:24:37 -0500

    Sort with separate page instead of JS

--- src/components/project-list/project-list.tsx
+++ src/components/project-list/project-list.tsx
@@ -1,16 +1,18 @@
-import { component$, useStore, useTask$ } from '@builder.io/qwik'
+import { component$, Slot, useStore, useTask$ } from '@builder.io/qwik'
 import { IProject, ITag } from '@marmadilemanteater/gh-static-site-lib/src/models/project'
 import UnifiedContentList from '../unified-content-list/unified-content-list'
 
 interface IProps {
   projects: Array<IProject>
   tagData: Array<ITag>
+  sortType?: string,
+  clientSideSorting?: boolean
 }
 
-export default component$(({ projects, tagData } : IProps) => {
+export default component$(({ projects, tagData, sortType = 'featured', clientSideSorting = true } : IProps) => {
   const state = useStore({
     projectsSorted: projects,
-    sortType: 'featured'
+    sortType: sortType
   })
 
   useTask$(({track}) => {
@@ -27,8 +29,11 @@ export default component$(({ projects, tagData } : IProps) => {
 
   return (
     <div class='project-list'>
-      <a class={`${state.sortType === 'featured'?'decoration-solid underline cursor-default':'cursor-pointer'} select-none p-4 inline-block`} onClick$={() => { state.sortType = 'featured' }}>Sort by featured</a>
-      <a class={`${state.sortType === 'lastUpdate'?'decoration-solid underline cursor-default':'cursor-pointer'} select-none p-4 inline-block`} onClick$={() => { state.sortType = 'lastUpdate' }}>Sort by last updated</a>
+      {clientSideSorting?<>
+        <a class={`${state.sortType === 'featured'?'decoration-solid underline cursor-default':'cursor-pointer'} select-none p-4 inline-block`} onClick$={() => { state.sortType = 'featured' }}>Sort by featured</a>
+        <a class={`${state.sortType === 'lastUpdate'?'decoration-solid underline cursor-default':'cursor-pointer'} select-none p-4 inline-block`} onClick$={() => { state.sortType = 'lastUpdate' }}>Sort by last updated</a>
+      </>:<></>}
+      <Slot />
       <UnifiedContentList {...{ content: state.projectsSorted, tagData: tagData, startIndex: 0 }} />
     </div>
   )
--- src/routes/projects/updated/index.tsx
+++ src/routes/projects/updated/index.tsx
@@ -2,16 +2,16 @@ import { component$ } from '@builder.io/qwik'
 import { DocumentHead, Link } from '@builder.io/qwik-city'
 import tags from '~/../data/tags.json'
 import projectsData from '~/../data/projects.json'
-import ProjectList from '../../components/project-list/project-list'
-import favicon from '../../images/favicon.ico'
+import ProjectList from '../../../components/project-list/project-list'
+import favicon from '../../../images/favicon.ico'
 import { IProject } from '@marmadilemanteater/gh-static-site-lib/src/models/project'
 export default component$(() => {
 
   return (
     <div class='bg-white dark:bg-zinc-900 rounded-t-xl border-t lg:border border-solid border-black'>
-      <ProjectList projects={projectsData as Array<IProject>} tagData={tags} clientSideSorting={false} >
-        <a class={'decoration-solid underline cursor-default select-none p-4 inline-block'} >Sort by featured</a>
-        <Link class={'cursor-pointer select-none p-4 inline-block'} href='/projects/updated'>Sort by last updated</Link>
+      <ProjectList projects={projectsData as Array<IProject>} tagData={tags} clientSideSorting={false} sortType='lastUpdate'>
+        <Link class={'cursor-pointer select-none p-4 inline-block'} href='/projects'>Sort by featured</Link>
+        <a class={'decoration-solid underline cursor-default select-none p-4 inline-block'} >Sort by last updated</a>
       </ProjectList>
     </div>
   )
