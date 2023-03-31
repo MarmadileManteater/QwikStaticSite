043d3d64ec5bed92940e903786a192bf1a19c7a9
Emma <MarmadileManteater@proton.me>
2023-03-31 18:20:20 -0400

    Load the video by default & only load godot when the user asks

--- src/components/godot-scene-background/godot-scene-background.tsx
+++ src/components/godot-scene-background/godot-scene-background.tsx
@@ -8,9 +8,9 @@ interface IProps {
 export default component$(({ sceneUrl } : IProps) => {
 
   return <>
-    <span class='z-10 md:absolute'>
+    <span class='z-10 top-2 left-0 md:absolute'>
       <a href='https://github.com/MarmadileManteater/JustThreeBlocksBackground' target='_blank' class='pt-2 pl-2 hover:underline text-white'>Background</a> <a class='pt-2 hover:underline text-white' href='https://godotengine.org/'>made with <Emoji emoji='🤖' />Godot</a>
     </span>
-    <iframe src={sceneUrl} class='w-screen h-screen fixed t-0 l-0 z-0'></iframe>
+    <iframe src={sceneUrl} class='w-screen h-screen fixed top-0 left-0 z-0'></iframe>
   </>
 })
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,28 +1,35 @@
-import { component$, Slot, useStore, useStyles$, useVisibleTask$ } from '@builder.io/qwik'
+import { $, component$, Slot, useStore, useStyles$, useVisibleTask$ } from '@builder.io/qwik'
 import Header from '../components/header/header'
 import highlightjs from 'highlight.js/styles/vs2015.css?inline'
 import Footer from '~/components/footer/footer'
 import GodotSceneBackground from '~/components/godot-scene-background/godot-scene-background'
 import InvidiousVideoBackground from '~/components/invidious-video-background/invidious-video-background'
+import Emoji from '~/components/emoji/emoji'
 
 export default component$(() => {
   useStyles$(highlightjs)
   const store = useStore({
-    loadGodot: false
+    loadGodot: false,
+    canLoadGodot: false
   })
   useVisibleTask$(() => {
-    if (!(('ontouchstart' in window) && window.innerWidth < 768)) {
-      // don't load the godot background on touch devices with small screens
-      // because they are more likely to be on mobile networks with a worse connection
-      // and more likely to load godot with a significant performance penalty
-      store.loadGodot = true
-    }
+    // don't load the godot background on touch devices with small screens
+    // because they are more likely to be on mobile networks with a worse connection
+    // and more likely to load godot with a significant performance penalty
+    store.canLoadGodot = !(('ontouchstart' in window) && window.innerWidth < 768)
+  })
+
+  const loadGodot = $(() => {
+    store.loadGodot = true
   })
 
   return (
     <>
-      <div class='flex flex-col min-h-screen' data-semi-transparent={store.loadGodot?'true':'false'}>
+      <div class='flex flex-col min-h-screen' data-semi-transparent='true'>
         <div class='wrapper flex-1 flex flex-col relative' >
+          {store.canLoadGodot&&!store.loadGodot?<>
+            <a class='fixed top-2 right-5 z-10 hover:underline cursor-pointer hidden lg:inline' onClick$={loadGodot}>Load <Emoji emoji='🤖' />Godot background scene in browser</a>
+          </>:<></>}
           {store.loadGodot?<>
             <GodotSceneBackground sceneUrl='/godot_background/index.html' />
           </>:<>
